fc30a4207937091fca379db16a37e482fb7dfd64
wizardOfOz21 <gasaevgamid@gmail.com>
2023-09-20 01:08:31 +0300

    Исправлен баг

--- impl.cpp
+++ impl.cpp
@@ -33,44 +33,6 @@ string coef(vector<string>& x, int n) {
     return "0";
 }
 
-string greater_fool(vector<string>& x, vector<string>& y) {
-    std::stringstream res;
-    int power = std::max(x.size(), y.size());
-    res << "(or ";
-    for (int i = power - 1; i >= 0; --i) {
-        res << "(and ";
-        for (int j = power - 1; j > i; --j) {
-            res << "(= " << coef(x, j) << " " << coef(y, j) << ")";
-        }
-        res << "(> " << coef(x, i) << " " << coef(y, i) << ")";
-        res << ")";
-    }
-    res << ")";
-    return res.str();
-}
-
-string equal_or_greater_fool(vector<string>& x, vector<string>& y) {
-    std::stringstream res;
-    int power = std::max(x.size(), y.size());
-    res << "(or ";
-    for (int i = power - 1; i >= 1; --i) {
-        res << "(and ";
-        for (int j = power - 1; j > i; --j) {
-            res << "(= " << coef(x, j) << " " << coef(y, j) << ")";
-        }
-        res << "(> " << coef(x, i) << " " << coef(y, i) << ")";
-        res << ")";
-    }
-    res << "(and ";
-    for (int j = power - 1; j > 0; --j) {
-        res << "(= " << coef(x, j) << " " << coef(y, j) << ")";
-    }
-    res << "(>= " << coef(x, 0) << " " << coef(y, 0) << ")";
-    res << ")";
-    res << ")";
-    return res.str();
-}
-
 string greater(vector<string>& x, vector<string>& y, bool equal_flag = false) {
     std::stringstream res;
     int power = std::max(x.size(), y.size());
@@ -101,8 +63,24 @@ string equal(vector<string>& x, vector<string>& y) {
 
 string make_condition(std::string lhs, std::string rhs, set& alf) {
     std::stringstream res;
-    Function rhs_f(rhs[0]);
-    Function lhs_f(lhs[0]);
+    char constr = rhs[0];
+    if (!alf.count(constr)) {
+        for (int i = 1; i < 5; ++i) {
+            res << "(declare-const " << param(constr, i) << " Int)\n";
+            res << "(assert (>= " << param(constr, i) << " 0))\n";
+        }
+        alf.insert(constr);
+    }
+    Function rhs_f(constr);
+    constr = lhs[0];
+    if (!alf.count(constr)) {
+        for (int i = 1; i < 5; ++i) {
+            res << "(declare-const " << param(constr, i) << " Int)\n";
+            res << "(assert (>= " << param(constr, i) << " 0))\n";
+        }
+        alf.insert(constr);
+    }
+    Function lhs_f(constr);
     for (int i = 1; i < rhs.length(); ++i) {
         char constr = rhs[i];
         if (!alf.count(constr)) {
