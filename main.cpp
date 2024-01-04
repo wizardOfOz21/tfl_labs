610256454ab2bb0b614bd8c8f62882c7316b122c
wizardOfOz <gasaevgamid@gmail.com>
2024-01-04 13:45:17 +0300

    Тестовые грамматики в отдельной папке

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(){
     // Grammar gr ("/Users/d.okutin/CLionProjects/tfl_labs/grammar.txt");
     // Grammar gr ("../simple_grammar.txt");
-    Grammar gr ("../ext_nformal_grammar.txt");
+    Grammar gr ("../grammars/ext_nformal_grammar.txt");
     SLRTable t(gr);
     t.printTable();
     LRParser parser(t);
